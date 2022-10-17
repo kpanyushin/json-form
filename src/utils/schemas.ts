f742dc04c26ec8d0c9ca91e38681c7482ae425f1
Kirill Panyushin <kirill.panyushin@workboard.com>
2022-10-17 20:38:52 +0200

    feat: add yup schema for json

---
+++ src/utils/schemas.ts
@@ -0,0 +1,53 @@
+import * as yup from "yup";
+const mapValues = require("lodash/mapValues");
+
+export const jsonSchema = yup
+  .object()
+  .shape({
+    title: yup.string().notRequired(),
+    desciption: yup.string().notRequired(),
+    properties: yup.lazy((obj) =>
+      yup
+        .object(
+          mapValues(obj, () =>
+            yup.object().shape({
+              type: yup
+                .mixed()
+                .oneOf(
+                  ["text", "number", "textarea", "date", "radio", "checkbox"],
+                  "Supported types of fields - text, number, textarea, date, radio, checkbox"
+                )
+                .required("Type of input is required"),
+              title: yup.string().required("Title is required"),
+              required: yup.boolean().notRequired(),
+              value: yup.mixed().notRequired(),
+              placeholder: yup.string().notRequired(),
+            })
+          )
+        )
+        .nullable()
+        .required("Form inputs required")
+    ),
+    buttons: yup.lazy((obj) =>
+    yup
+      .object(
+        mapValues(obj, () =>
+          yup.object().shape({
+            type: yup
+              .mixed()
+              .oneOf(
+                ["submit", "reset", "button"],
+                "Supported types of buttons - submit, reset, button"
+              )
+              .required("Type of button is required. At least one should be submit"),
+            title: yup.string().required("Title is required"),
+            required: yup.boolean().notRequired(),
+            value: yup.mixed().notRequired(),
+            placeholder: yup.string().notRequired(),
+          })
+        )
+      )
+      .nullable()
+  ),
+  })
+  .nullable();
